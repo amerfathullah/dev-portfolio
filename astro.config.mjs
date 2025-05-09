07f46a0903f7418925705c2eac6fe540138deae5
amerfathullah <amer@tfx.global>
2025-05-09 15:00:09 +0800

    improve performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: true, // Enables HTML minification
+  build: {
+    inlineStylesheets: 'auto' // Helps reduce render-blocking CSS
+  },
   vite: {
+    build: {
+      minify: 'terser', // Use Terser for more aggressive minification
+      terserOptions: {
+        compress: {
+          drop_console: true, // Remove console.logs
+          passes: 2 // Multiple optimization passes
+        }
+      }
+    },
     plugins: [tailwindcss()],
   }
 });
