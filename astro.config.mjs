703596fc4fdaff4f031f290c14c7b06f293e578d
amerfathullah <amer@tfx.global>
2025-05-15 13:19:38 +0800

    fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.amerfathullah.com/",
+  site: "https://www.amerfathullah.com",
   // Enables HTML minification
   compressHTML: true,
   build: {
